d6fee3e7bcaf076d597bca050f1350a080bd750b
martin.cervenka@eurowag.com <martin.cervenka@eurowag.com>
2021-04-15 15:43:27 +0200

    cleaned code

--- src/main/java/cz/pumpitup/pn5/starterkit/Firefox.java
+++ src/main/java/cz/pumpitup/pn5/starterkit/Firefox.java
@@ -2,7 +2,7 @@ package cz.pumpitup.pn5.starterkit;
 
 import cz.pumpitup.pn5.core.webdriver.Capability;
 import cz.pumpitup.pn5.web.WebApplication;
-import cz.pumpitup.pn5.starterkit.pages.SearchPage;
+import cz.pumpitup.pn5.starterkit.Google.pages.SearchPage;
 
 
 @Capability(key = "browserName", value = "firefox")
--- src/test/java/cz/pumpitup/pn5/starterkit/SingleTest.java
+++ src/test/java/cz/pumpitup/pn5/starterkit/SingleTest.java
@@ -1,14 +1,12 @@
 package cz.pumpitup.pn5.starterkit;
 
 import org.junit.jupiter.api.Test;
-import cz.pumpitup.pn5.web.WebApplication;
 
 public class SingleTest {
 
     @Test
-    //public void test(Google google) {
-    public void test(Firefox google) {
-        google.openSearchPage()
+    public void test(Firefox firefox) {
+        firefox.openSearchPage()
                     .AcceptCookies()
                     .typeIntoSearchBox("BrowserStack")
                     .submit()
